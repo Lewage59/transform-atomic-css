50468e95d6c569e63513c4dcc87d6b30b4a032c8
Lewage59 <920576696@qq.com>
2022-07-28 00:24:25 +0800

    feat: rollup config for build

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,8 @@
 import json from 'rollup-plugin-json';
+import typescript from '@rollup/plugin-typescript';
+import nodeResolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import babel from 'rollup-plugin-babel'
 
 export default {
   input: 'src/index.ts',
@@ -6,5 +10,14 @@ export default {
     file: 'dist/bundle.js',
     format: 'cjs'
   },
-  plugins: [ json() ]
+  plugins: [
+    json(),
+    typescript(),
+    nodeResolve(),
+    commonjs(),
+    babel({
+      exclude: 'node_modules/**'
+    })
+  ],
+  external: ['css', 'dom-serializer', 'htmlparser2', 'less', 'lodash', 'minimatch'],
 };
