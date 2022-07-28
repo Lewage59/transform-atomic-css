92ac9e5dae6d625b78501fc1cba382537ac1111e
Lewage59 <920576696@qq.com>
2022-07-28 23:33:06 +0800

    feat: terser bundle

--- rollup.config.js
+++ rollup.config.js
@@ -3,6 +3,7 @@ import typescript from '@rollup/plugin-typescript';
 import nodeResolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import babel from 'rollup-plugin-babel'
+import { terser } from 'rollup-plugin-terser';
 
 export default {
   input: 'src/index.ts',
@@ -17,7 +18,8 @@ export default {
     commonjs(),
     babel({
       exclude: 'node_modules/**'
-    })
+    }),
+    terser(),
   ],
   external: ['css', 'dom-serializer', 'htmlparser2', 'less', 'lodash', 'minimatch'],
 };
