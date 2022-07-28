91eefc73cb24ca802522a7fdeda66e27ff1a3fc9
Lewage59 <920576696@qq.com>
2022-07-29 00:39:11 +0800

    chore: build config

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,5 @@
 import json from 'rollup-plugin-json';
-import typescript from '@rollup/plugin-typescript';
+import typescript from 'rollup-plugin-typescript2';
 import nodeResolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import babel from 'rollup-plugin-babel'
@@ -13,9 +13,11 @@ export default {
   },
   plugins: [
     json(),
-    typescript(),
-    nodeResolve(),
+    typescript({
+      useTsconfigDeclarationDir: true
+    }),
     commonjs(),
+    nodeResolve(),
     babel({
       exclude: 'node_modules/**'
     }),
