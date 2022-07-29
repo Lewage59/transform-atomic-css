9949a2b2c553fcc7028a3c4475a8d748e1f9a1c4
Lewage59 <920576696@qq.com>
2022-07-29 13:33:17 +0800

    chore: package config

--- rollup.config.js
+++ rollup.config.js
@@ -5,10 +5,12 @@ import commonjs from '@rollup/plugin-commonjs';
 import babel from 'rollup-plugin-babel'
 import { terser } from 'rollup-plugin-terser';
 
+const pkg = require('./package.json')
+
 export default {
   input: 'src/index.ts',
   output: {
-    file: 'dist/bundle.js',
+    file: pkg.main,
     format: 'cjs'
   },
   plugins: [
